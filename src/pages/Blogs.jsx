a057302e2da15980c156d1693fa08aa88133db18
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-08-18 16:43:34 +0200

    Add page blogs

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,57 @@
+import './Pages.scss'
+import { Link } from 'react-router-dom';
+
+import blogs from '../assets/data/blogs'
+import arrangments from '../assets/data/arrangment';
+
+import CommonSection from '../components/CommonSection/CommonSection'
+import BlogItem from '../components/Blog/BlogItem'
+import ArrangmentItem from '../components/Arrangment/ArrangmentItem'
+
+const Blogs = () => {
+  return (
+    <>
+        <div className="main-blog">
+            <div className="container">
+                <div className="main-blog-content">
+                    <CommonSection title='Blog' />
+                    <div className="main-blog-items">
+                        {blogs?.map((blog, index) => (
+                                <BlogItem key={index} blog={blog} />
+                            ))}
+                    </div>
+                    <div className="pagination">
+                        <div className="pagination-content">
+                            <span className="pagination-item active">1</span>
+                            <Link href="#" className="pagination-item">2</Link>
+                            <Link  href="" className="pagination-item">3</Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div className="page-arrangment">
+            <div className="container">
+                <CommonSection title='Aranżacje Wnętrz' />
+                <div className="page-arrangment-content">
+                    <div className="page-arrangment-items">
+                        {arrangments?.map((arrangment) => (
+                            <ArrangmentItem key={arrangment.id} arrangment={arrangment} />
+                        ))}
+                    </div>
+                    <Link to="/aranzacje" class="page-arrangment-all">zobacz wszystkie aranżacje</Link>
+                </div>
+            </div>
+        </div>
+
+
+    
+    </>
+
+    
+  )
+}
+
+export default Blogs
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from "react-router-dom"
 import Home from "../pages/Home"
+import Blogs from "../pages/Blogs"
 
 const Routers = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/blogs" element={<Blogs/>} />
     </Routes>
  
   )
